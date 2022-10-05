a6b55dc2897ca31021afdcfcd873695c3550ba27
Sneha Madle <snehamadle@Snehas-Air.lan>
2022-10-05 00:02:54 -0400

    Correct minor errors in auth

--- frontend/src/auth/helper/index.js
+++ frontend/src/auth/helper/index.js
@@ -2,25 +2,25 @@ import { API } from "../../backend";
 
 export const signup = user => {
     return fetch(`${API}/signup`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(user)
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
     })
-      .then(response => {
-        return response.json();
-      })
-      .catch(err => console.log(err));
-  };
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
 
 export const signin = user => {
     return fetch(`${API}/signin`, {
         method: "POST",
         headers:{
             Accept: "application/json",
-            "Content-Type": "aplication/json"
+            "Content-Type": "application/json"
         },
         body: JSON.stringify(user)
     })
@@ -30,7 +30,7 @@ export const signin = user => {
     .catch(err => console.log(err));
 };
 
-export const signout = next => {
+export const Signout = next => {
     if(typeof window !== 'undefined'){
         localStorage.removeItem("jwt");
         next();
